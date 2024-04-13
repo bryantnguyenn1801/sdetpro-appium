7f5293108c3199ec2e6afddd771af8ff9862f19a
bryantnguyen1801 <binh.nguyen@setel.com>
2024-04-13 11:24:23 +0700

    Bryant's lab_day_10_2 update

--- src/labs/day_10/day_10_2/ContractEmployee.java
+++ src/labs/day_10/day_10_2/ContractEmployee.java
@@ -4,7 +4,7 @@ package labs.day_10.day_10_2;
 public class ContractEmployee extends Employee {
     @Override
     public int getSalary() {
-        return this.salary = 40000;
+        return this.salary = 30000;
     }
 
     @Override
